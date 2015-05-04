d6326e5467ccbcc272f3a162ed6a85723aa33bf7
rahul1346 <rahulsharma2001@gmail.com>
2015-05-04 17:57:24 -0400

    refactored

--- read_curriculum.py
+++ read_curriculum.py
@@ -1,55 +1,10 @@
 from __future__ import print_function
 from os.path import join, dirname, abspath
 import xlrd
-from read_curriculum import read_curriculum
 from xlrd.sheet import ctype_text
 
 
-
-class crs(object):
-
-
-    fname = join(dirname(dirname(abspath(__file__))), 'crs', 'sampleStudent.xls')
-    #open the workbook
-
-    xl_workbook = xlrd.open_workbook(fname)
-    
-    sheet_names = xl_workbook.sheet_names()
-
-    # print ('Sheet Names', sheet_names)
-
-    # print ('hello')
-
-    xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
-    
-    row = xl_sheet.row(0) 
-
-    # print (type (row))
-
-    # print ('(Column # ) type:value')
-    for idx, cell_obj in enumerate(row):
-        cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')
-        # print ('(%s) %s %s ' % (idx, cell_type_str, cell_obj.value))
-
-    #print columns 0,1,2,3,5
-
-  #  listOfColumnIndices = [0,1,2,3,5]
-    listOfColumnIndices = [0,5]
-
-    col = {}
-    
-    for row_idx in range(1, xl_sheet.nrows):
-        #print ('-'*40)
-        #print ('Row: %s ' % row_idx) #print row number
-        cell_obj_key = xl_sheet.cell(row_idx, 0)
-        cell_obj_val = xl_sheet.cell(row_idx, 5)
-        #print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
-        col[cell_obj_key] = cell_obj_val
-
-
-
-    print (col)
-
+def read_curriculum():  
     cname = join(dirname(dirname(abspath(__file__))), 'crs', 'EE-curriculum-October2014 (11).xls')
     cl_workbook = xlrd.open_workbook(cname)
 
@@ -121,6 +76,3 @@ class crs(object):
 
 
     print (list_of_courses)
-
-read_curriculum()
-crs()
