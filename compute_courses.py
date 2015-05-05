87dd632e8b2c148495bfa1eedf8a017722506f4f
rahul1346 <rahulsharma2001@gmail.com>
2015-05-05 16:37:44 -0400

    added fetch_class fxn

--- compute_courses.py
+++ compute_courses.py
@@ -1,7 +1,6 @@
 import unicodedata
 
 
-
 def compute_failed_courses(student_grades):
     list_of_failed_courses = []
     for key,value in student_grades.iteritems():
@@ -20,11 +19,42 @@ def compute_failed_courses(student_grades):
     list_of_failed_courses.sort(key=lambda x: x.split(" ")[-1])
     return list_of_failed_courses
 
+def process_curriculum(curriculum):
+    # pass
+    curriculum_str = []
+    for key in curriculum:
+        # print (type (key), key)
+        curriculum_str.append(unicodedata.normalize('NFKD', key).encode('ascii','ignore'))
+
+    return curriculum_str
+
+def fetch_class(curriculum, student_grades):
+    return None
+
 def compute_courses(student_grades, curriculum) :
     #print (student_grades)
-    #print (curriculum)
+    # print (curriculum)
+    list_of_rec_courses = []
     list_of_failed_courses = compute_failed_courses(student_grades)
-#    print (list_of_failed_courses[:6])
-    if (len(list_of_failed_courses) >=6):
+    # print (list_of_failed_courses[:6])
+    curriculum_str = process_curriculum(curriculum)
+    # print (curriculum_str)
+
+    if (len(list_of_failed_courses) >=10):
         return list_of_failed_courses[:6]
+
+    list_of_rec_courses = list_of_failed_courses
+
+    num_of_additional_courses_to_propose = 10 - len(list_of_rec_courses) 
+
+    for i in range(num_of_additional_courses_to_propose):
+        list_of_rec_courses.append(fetch_class(curriculum, student_grades))
+
+    return list_of_rec_courses
+
+
+
+
+
+
     
--- read_curriculum.py
+++ read_curriculum.py
@@ -10,9 +10,9 @@ def read_curriculum():
 
     sheet_names = cl_workbook.sheet_names()
 
-    print ('Sheet Names', sheet_names)
+    # print ('Sheet Names', sheet_names)
 
-    print ('hello')
+    # print ('hello')
 
     cl_sheet = cl_workbook.sheet_by_name(sheet_names[0])
 
@@ -20,25 +20,25 @@ def read_curriculum():
 
     for idx, cell_obj in enumerate(row):
         cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')
-        print ('(%s) %s %s ' % (idx, cell_type_str, cell_obj.value))
+        # print ('(%s) %s %s ' % (idx, cell_type_str, cell_obj.value))
 
 
     num_cols = cl_sheet.ncols
     list_of_courses = []
 
     for row_idx in range(1, cl_sheet.nrows):
-        print ('-'*40)
-        print ('Row: %s ' % row_idx) #print row number
+        # print ('-'*40)
+        # print ('Row: %s ' % row_idx) #print row number
         for col_idx in range(0, num_cols):  # Iterate through columns
             cell_obj = cl_sheet.cell(row_idx, col_idx)  # Get cell object by row, col
-            print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
+            # print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
 
      
     for row_idx in range(7, 14):
             #print ('-'*40)
             #print ('Row: %s ' % row_idx) #print row number
-            list_of_courses.append(cl_sheet.cell(row_idx,0))
-            list_of_courses.append(cl_sheet.cell(row_idx,7))
+            list_of_courses.append(cl_sheet.cell(row_idx,0).value)
+            list_of_courses.append(cl_sheet.cell(row_idx,7).value)
 
             #print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
 
@@ -47,8 +47,8 @@ def read_curriculum():
     for row_idx in range(19, 25):
             #print ('-'*40)
             #print ('Row: %s ' % row_idx) #print row number
-            list_of_courses.append(cl_sheet.cell(row_idx,0))
-            list_of_courses.append(cl_sheet.cell(row_idx,7))
+            list_of_courses.append(cl_sheet.cell(row_idx,0).value)
+            list_of_courses.append(cl_sheet.cell(row_idx,7).value)
             
             #print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
 
@@ -57,8 +57,8 @@ def read_curriculum():
     for row_idx in range(30, 36):
             #print ('-'*40)
             #print ('Row: %s ' % row_idx) #print row number
-            list_of_courses.append(cl_sheet.cell(row_idx,0))
-            list_of_courses.append(cl_sheet.cell(row_idx,7))
+            list_of_courses.append(cl_sheet.cell(row_idx,0).value)
+            list_of_courses.append(cl_sheet.cell(row_idx,7).value)
             
             #print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
 
@@ -67,12 +67,12 @@ def read_curriculum():
     for row_idx in range(41, 46):
             #print ('-'*40)
             #print ('Row: %s ' % row_idx) #print row number
-            list_of_courses.append(cl_sheet.cell(row_idx,0))
-            list_of_courses.append(cl_sheet.cell(row_idx,7))
+            list_of_courses.append(cl_sheet.cell(row_idx,0).value)
+            list_of_courses.append(cl_sheet.cell(row_idx,7).value)
             
             #print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
 
-    list_of_courses.append(cl_sheet.cell(15,7))
+    list_of_courses.append(cl_sheet.cell(15,7).value)
 
 
     # print (list_of_courses)
